738ce2898c0253ef847db8ede9263151acba61d1
phoenix104104 <phoenix104104@gmail.com>
2017-04-10 19:05:08 -0700

    update bash script for ubuntu

--- datasets/download_SR_datasets.sh
+++ datasets/download_SR_datasets.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget http://vllab1.ucmerced.edu/~wlai24/LapSRN/results/SR_training_datasets.zip
 unzip SR_training_datasets.zip
 wget http://vllab1.ucmerced.edu/~wlai24/LapSRN/results/SR_testing_datasets.zip

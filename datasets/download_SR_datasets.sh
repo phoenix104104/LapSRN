bd54bb59379ccd8927c224f2b823a36c430687ec
phoenix104104 <phoenix104104@gmail.com>
2017-04-10 19:00:02 -0700

    add dataset download script

---
+++ datasets/download_SR_datasets.sh
@@ -0,0 +1,4 @@
+wget http://vllab1.ucmerced.edu/~wlai24/LapSRN/results/SR_training_datasets.zip
+unzip SR_training_datasets.zip
+wget http://vllab1.ucmerced.edu/~wlai24/LapSRN/results/SR_testing_datasets.zip
+unzip SR_testing_datasets.zip

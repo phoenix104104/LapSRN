789e5dcf2657823972c7643c32d414be981550f0
Jason Lai <phoenix104104@gmail.com>
2017-07-17 21:12:21 -0700

    remove copy command

--- install.m
+++ install.m
@@ -1,11 +1,11 @@
 
-if isunix || ismac
-    cmd = 'cp -a vllab_dag_loss.m matconvnet/matlab/+dagnn';
-else % ispc
-    cmd = 'copy vllab_dag_loss.m matconvnet/matlab/+dagnn';
-end
-fprintf('%s\n', cmd);
-system(cmd);
+%if isunix || ismac
+%    cmd = 'cp -a vllab_dag_loss.m matconvnet/matlab/+dagnn';
+%else % ispc
+%    cmd = 'copy vllab_dag_loss.m matconvnet/matlab/+dagnn';
+%end
+%fprintf('%s\n', cmd);
+%system(cmd);
 
 
 addpath('matconvnet/matlab');
